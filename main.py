ae0293254f621c9fe0508608ad44fea286215527
SaraFarron <egu7pC6bsPtPpDz>
2021-04-06 12:56:00 +0700

    work in progress...

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+
+url = "https://dtf.ru/games/693406-ya-vnimatelno-slushal-no-malo-chto-ponyal-edzi-sinkava-rasskazal-o-rabote-nad-death-stranding?comment=10016724"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+soup = BeautifulSoup(html, "html.parser")
+res = [tag for tag in soup.find_all("div", {"class": "comments__item__content"})]
+print(res[0].findChildren())
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 #### Parser, that collects comments from DTF post
 This thing has no practical use since DTF already has API for that, 
 but I want to practice my parse skills.
+```pip install BeautifulSooup```
