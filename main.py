dd61e53e816006f7eac7ec362cd1a0cf5a8a4f91
SaraFarron <egu7pC6bsPtPpDz>
2021-04-08 14:20:51 +0700

    Fixed children comments

--- main.py
+++ main.py
@@ -3,41 +3,27 @@ from urllib.request import urlopen
 
 
 def create_comment(comment_soup):
-    children_comments = []
-    for comment in comment_soup.find("div", class_="comments__item"):
-        try:
-            children_comments.append({
-                "author": comment.find("p", class_="comments__item__user__name").get_text(),
-                "text": comment.find("div", class_="comments__item__text").get_text(),
-                "child_comments": create_comment(comment.find("div", class_="comments__item_children"))
-            })
-        except AttributeError:
-            children_comments.append({
-                "author": comment.find("p", class_="comments__item__user__name").get_text(),
-                "text": comment.find("div", class_="comments__item__text").get_text(),
-            })
+    children = []
+    try:
+        for child in comment_soup.find("div", class_="comments__item__children").find_all("div", class_="comments__item"):
+            children.append(create_comment(child))
+    except TypeError or AttributeError:
+        pass
+    children_comments = {
+        "author": comment_soup.find("p", class_="comments__item__user__name").get_text(),
+        "text": comment_soup.find("div", class_="comments__item__text").get_text(),
+        "child_comments": children
+    }
     return children_comments
 
 
-url = "https://dtf.ru/games/693406-ya-vnimatelno-slushal-no-malo-chto-ponyal-edzi-sinkava-rasskazal-o-rabote-nad-death-stranding?comment=10016724"
+url = "https://dtf.ru/games/696984-gruppa-zashchity-musulman-prizyvaet-valve-zapretit-prodazhu-igry-six-days-in-fallujah"
 page = urlopen(url)
 html = page.read().decode("utf-8")
 
 soup = BeautifulSoup(html, "html.parser")
 comments_soup = [tag for tag in soup.find_all("div", {"class": "comments__item"})]
-comments = []
-for comment in comments_soup:
-    try:
-        comments.append({
-            "author": comment.find("p", class_="comments__item__user__name").get_text(),
-            "text": comment.find("div", class_="comments__item__text").get_text(),
-            "child_comments": create_comment(comment.find("div", class_="comments__item_children"))
-        })
-    except AttributeError:
-        comments.append({
-            "author": comment.find("p", class_="comments__item__user__name").get_text(),
-            "text": comment.find("div", class_="comments__item__text").get_text(),
-        })
+comments = [create_comment(comment) for comment in comments_soup]
 
 for item in comments:
     print(item)
